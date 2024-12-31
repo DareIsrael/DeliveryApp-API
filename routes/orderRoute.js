3d724c734187d43e8d128434003a002a8e74824a
DareIsrael <dareisrael4@gmail.com>
2024-12-31 01:04:24 -0800

    trying

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
-// orderRouter.post("/verify", verifyOrder);
+orderRouter.post("/verify", verifyOrder);
 orderRouter.post("/userorders", authMiddleware, userOrder);
 orderRouter.get("/list", listOrders);
 orderRouter.post("/status", updateStatus);
